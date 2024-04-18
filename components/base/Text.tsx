5a4b0eacfa5f353bb68cc0c022b7a88ffa1f46ea
“Petter <petter@skaidigital.com>
2024-04-18 10:48:54 +0200

    feat: added retailers page

--- components/base/Text.tsx
+++ components/base/Text.tsx
@@ -6,6 +6,7 @@ const textProps = cva({
   variants: {
     size: {
       sm: 'text-sm placeholder:text-sm',
+      md: 'text-md placeholder:text-md',
       lg: 'text-lg placeholder:text-lg',
       eyebrow: 'text-eyebrow uppercase placeholder:text-eyebrow',
       default: 'text-paragraph-sm lg:text-paragraph-lg placeholder:text-sm lg:placeholder:text-lg'
