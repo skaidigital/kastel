7cc9071cff058b7643e0102f11f3a501e3899a92
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-04-22 13:35:41 +0200

    feat(product card): more wip styling

--- components/shared/ProductCard/index.tsx
+++ components/shared/ProductCard/index.tsx
@@ -1,5 +1,6 @@
 // 'use client';
 
+import { formatPrice } from '@/app/api/shopify/utils';
 import { Badge } from '@/components/Badge';
 import { CustomLink } from '@/components/CustomLink';
 import { SanityImage } from '@/components/sanity/SanityImage';
@@ -9,6 +10,7 @@ import { Rating, RatingFallback } from '@/components/shared/ProductCard/Rating';
 import { Wishlist, WishlistFallback } from '@/components/shared/ProductCard/Wishlist';
 import { ROUTES } from '@/data/constants';
 import { ProductCardProps } from '@/lib/sanity/types';
+import { Money } from '@/lib/shopify/types';
 import { cn } from '@/lib/utils';
 import { Suspense } from 'react';
 
@@ -16,7 +18,6 @@ interface Props extends ProductCardProps {
   firstImage?: 'product' | 'lifestyle';
   priority?: boolean;
   className?: string;
-  slugPrefix?: string;
 }
 
 // TODO consider having a container for the badges and rating so that we can align them
@@ -27,7 +28,6 @@ export function ProductCard({
   lifestyleImage,
   title,
   className,
-  slugPrefix = 'products',
   badges,
   priority
 }: Props) {
@@ -47,54 +47,64 @@ export function ProductCard({
   const isOnSale = true;
   const discountPercentage = 20;
 
+  const sizeRange = '34-42';
+
+  const price: Money = {
+    amount: '100.00',
+    currencyCode: 'USD'
+  };
+  const formattedPrice = formatPrice(price);
+
   return (
-    <article
+    <CustomLink
       // onMouseEnter={() => hasHoverImage && setIsHovered(true)}
       // onMouseLeave={() => hasHoverImage && setIsHovered(false)}
-      className={cn('group flex w-full shrink-0 flex-col @container', className)}
+      className={cn('group border-b border-brand-light-grey @container', className)}
+      href={`${ROUTES.PRODUCTS}/${slug}`}
     >
-      <CustomLink href={`${ROUTES.PRODUCTS}/${slug}`}>
-        <ImageContainer>
-          <div className="absolute right-3 top-3 z-50 @[320px]:right-4 @[320px]:top-4">
-            <Suspense fallback={<WishlistFallback />}>
-              <Wishlist sku="123" />
-            </Suspense>
-          </div>
-          {badges && (
-            <div className="absolute right-2 top-3 z-10 flex flex-col gap-1 xl:flex-row">
-              {badges.map((badge) => (
-                <Badge key={badge} className="mr-2" size="sm" variant="neutral">
-                  {badge}
-                </Badge>
-              ))}
-            </div>
-          )}
-          {chosenFirstImage && (
-            <SanityImage
-              // image={isHovered && hasLifestyleImage ? chosenHoverImage : chosenFirstImage}
-              image={chosenFirstImage}
-              className={cn('scale-100 rounded-project object-cover')}
-              sizes="(min-width: 640px) 50vw, 25vw"
-              fill
-              priority={priority}
-            />
-          )}
-          <div className="absolute bottom-3 left-3 flex gap-x-1 @[320px]:bottom-4 @[320px]:left-4">
-            {isOnSale && discountPercentage && <Badge className="">-{discountPercentage}%</Badge>}
-            <Badge className="">New</Badge>
-          </div>
-          <Suspense fallback={<RatingFallback />}>
-            <Rating sku="123" />
+      <ImageContainer>
+        <div className="absolute right-3 top-3 z-50 @[320px]:right-4 @[320px]:top-4">
+          <Suspense fallback={<WishlistFallback />}>
+            <Wishlist sku="123" />
           </Suspense>
-        </ImageContainer>
-        <div className="flex flex-col justify-center gap-y-2 bg-white p-3 @[320px]:p-4">
-          <div className="flex flex-col gap-y-1">
-            <h3 className="text-xs @[320px]:text-sm">{title}</h3>
-            <span className="text-xs text-brand-mid-grey @[320px]:text-sm">size og price</span>
-          </div>
-          <Colors />
         </div>
-      </CustomLink>
-    </article>
+        {badges && (
+          <div className="absolute right-2 top-3 z-10 flex flex-col gap-1 xl:flex-row">
+            {badges.map((badge) => (
+              <Badge key={badge} className="mr-2" size="sm" variant="neutral">
+                {badge}
+              </Badge>
+            ))}
+          </div>
+        )}
+        {chosenFirstImage && (
+          <SanityImage
+            // image={isHovered && hasLifestyleImage ? chosenHoverImage : chosenFirstImage}
+            image={chosenFirstImage}
+            className={cn('scale-100 rounded-project object-cover')}
+            sizes="(min-width: 640px) 50vw, 25vw"
+            fill
+            priority={priority}
+          />
+        )}
+        <div className="absolute bottom-3 left-3 flex gap-x-1 @[320px]:bottom-4 @[320px]:left-4">
+          {isOnSale && discountPercentage && <Badge className="">-{discountPercentage}%</Badge>}
+          <Badge className="">New</Badge>
+        </div>
+        <Suspense fallback={<RatingFallback />}>
+          <Rating sku="123" />
+        </Suspense>
+      </ImageContainer>
+      <div className="flex flex-col justify-center gap-y-2 bg-white p-3 @[320px]:p-4">
+        <div className="mb-2 flex flex-col gap-y-1">
+          <h3 className="truncate text-xs @[320px]:text-sm">{title}</h3>
+          <div className="flex gap-x-3 text-xs text-brand-mid-grey @[320px]:gap-x-4 @[320px]:text-sm">
+            <span>{formattedPrice}</span>
+            <span>{sizeRange}</span>
+          </div>
+        </div>
+        <Colors />
+      </div>
+    </CustomLink>
   );
 }
