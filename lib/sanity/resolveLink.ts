e292f8bf3388c1809cdca91e094280a05329fa63
“Petter <petter@skaidigital.com>
2024-04-28 20:54:40 +0200

    feat: reworked resolveLink to build

--- lib/sanity/resolveLink.ts
+++ lib/sanity/resolveLink.ts
@@ -2,10 +2,20 @@ import { resolveHref } from '@/lib/sanity/resolveHref';
 import { LinkProps, LinkWithoutTextProps } from '@/lib/sanity/types';
 
 export function resolveLink(link: LinkProps | LinkWithoutTextProps) {
-  const href =
-    link.linkType === 'external'
-      ? link.href
-      : resolveHref({ slug: link.linkTo.slug, type: link.linkTo.type }) || '#';
+  // const href =
+  //   link.linkType === 'external'
+  //     ? link.href
+  //     : resolveHref({ slug: link.linkTo.slug, type: link.linkTo.type }) || '#';
 
-  return href;
+  switch (link.linkType) {
+    case 'internal':
+      return resolveHref({ slug: link.linkTo.slug, type: link.linkTo.type });
+    case 'external':
+      return link.href;
+    case 'smile':
+      return `#`;
+    default:
+      return '#';
+  }
+  // return href;
 }
