1b6017d4b94582d9d1a1ed2b5d7e4bff25e26cb1
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-05-01 21:43:19 +0200

    feat(payment providers): fix icon rendering

--- components/shared/PaymentProviders/index.tsx
+++ components/shared/PaymentProviders/index.tsx
@@ -1,7 +1,17 @@
-import { Icon } from '@/components/Icon';
+import { AMEXLogo } from '@/components/icons/AMEXLogo';
+import { AmazonPayLogo } from '@/components/icons/AmazonPayLogo';
+import { ApplePayLogo } from '@/components/icons/ApplePayLogo';
+import { GooglePayLogo } from '@/components/icons/GooglePayLogo';
+import { MasterCardLogo } from '@/components/icons/MasterCardLogo';
+import { MobilePayLogo } from '@/components/icons/MobilePayLogo';
+import { PayPalLogo } from '@/components/icons/PayPalLogo';
+import { SamsungPayLogo } from '@/components/icons/SamsungPayLogo';
+import { VippsLogo } from '@/components/icons/VippsLogo';
+import { VisaLogo } from '@/components/icons/VisaLogo';
 import { loadPaymentProviders } from '@/components/shared/PaymentProviders/hooks';
-import { MarketValues } from '@/data/constants';
+import { MarketValues, PaymentProviderType } from '@/data/constants';
 import { cn } from '@/lib/utils';
+import React from 'react';
 
 interface Props {
   market: MarketValues;
@@ -15,21 +25,38 @@ export async function PaymentProviders({ market, size = 'md' }: Props) {
 
   return (
     <div className={cn('flex', size === 'sm' ? 'gap-x-1' : 'gap-x-2')}>
-      {paymentProviders.data.map((provider, index) => (
-        <div className="inline-block" key={index}>
-          <Icon
-            viewBox="0 0 70 48"
-            className={cn(
-              'h-auto',
-              size === 'sm' && 'w-8',
-              size === 'md' && 'w-12',
-              size === 'lg' && 'w-16'
-            )}
-            name={provider}
-            id={provider}
-          />
-        </div>
+      {paymentProviders.data.map((provider) => (
+        <React.Fragment key={provider}>
+          {getPaymentProvider(provider, 'h-8 w-auto rounded-[4px] border border-brand-light-grey')}
+        </React.Fragment>
       ))}
     </div>
   );
+}
+
+function getPaymentProvider(provider: PaymentProviderType, className?: string) {
+  switch (provider) {
+    case 'amazonPay':
+      return <AmazonPayLogo className={className} />;
+    case 'amex':
+      return <AMEXLogo className={className} />;
+    case 'applePay':
+      return <ApplePayLogo className={className} />;
+    case 'googlePay':
+      return <GooglePayLogo className={className} />;
+    case 'masterCard':
+      return <MasterCardLogo className={className} />;
+    case 'mobilePay':
+      return <MobilePayLogo className={className} />;
+    case 'payPal':
+      return <PayPalLogo className={className} />;
+    case 'samsungPay':
+      return <SamsungPayLogo className={className} />;
+    case 'vipps':
+      return <VippsLogo className={className} />;
+    case 'visa':
+      return <VisaLogo className={className} />;
+    default:
+      return null;
+  }
 }
