54c7f22c9f48e81b7a3a3625a32165fcaf93e1c4
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-05-09 16:48:11 +0200

    feat(collection page): remove x overflow

--- components/pages/CollectionPage/CollectionLayout.tsx
+++ components/pages/CollectionPage/CollectionLayout.tsx
@@ -208,7 +208,7 @@ export function CollectionGrid({
   return (
     <div
       className={cn(
-        '-gap-[1px]',
+        'overflow-hidden',
         number === 1 && 'grid grid-cols-1 lg:grid-cols-4',
         number === 2 && 'grid grid-cols-2 lg:grid-cols-4',
         number === 3 && 'grid grid-cols-2 lg:grid-cols-3',
