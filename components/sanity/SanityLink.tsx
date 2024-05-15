b98100286bb170bff32092d3c0d9f9fbaaab20eb
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-05-15 10:48:10 +0200

    feat(mobile menu): fix it not closing when clicking buttons in sanitylink

--- components/sanity/SanityLink.tsx
+++ components/sanity/SanityLink.tsx
@@ -10,17 +10,30 @@ interface Props {
   link: LinkProps | LinkWithoutTextProps;
   children: ReactNode;
   className?: string;
-  onClick?: () => void;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 }
 
 export const SanityLink = ({ link, children, className, onClick }: Props) => {
   if (!link) return null;
 
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    // Call the provided onClick handler if available
+    if (onClick) {
+      onClick(event);
+    }
+
+    if (link.linkType === 'smile' && window.SmileUI) {
+      event.preventDefault(); // Prevent default link behavior
+      window.SmileUI.openPanel({ deep_link: link.smileLauncher }).catch(() => {
+        console.warn('SmileUI is not loaded and initialized.');
+      });
+    }
+  };
+
   if (link.linkType === 'internal') {
     const slug = getSlug(link);
-
     return (
-      <CustomLink onClick={onClick} className={className || ''} href={slug || '#'}>
+      <CustomLink onClick={handleClick} className={className || ''} href={slug || '#'}>
         {children}
       </CustomLink>
     );
@@ -30,7 +43,7 @@ export const SanityLink = ({ link, children, className, onClick }: Props) => {
     return (
       <Link
         className={className || ''}
-        onClick={onClick}
+        onClick={handleClick}
         href={link.href}
         target={link.openInNewTab ? '_blank' : '_self'}
       >
@@ -39,22 +52,10 @@ export const SanityLink = ({ link, children, className, onClick }: Props) => {
     );
   }
 
+  // Handle other link types like 'smile'
   if (link.linkType === 'smile') {
-    const deeplinkType = link.smileLauncher;
-
     return (
-      <button
-        className={className || ''}
-        onClick={async () => {
-          if (window.SmileUI) {
-            await window.SmileUI.openPanel({ deep_link: deeplinkType });
-            // onClick?.();
-          } else {
-            console.warn('SmileUI is not loaded and initialized.');
-            // onClick?.();
-          }
-        }}
-      >
+      <button className={className || ''} onClick={handleClick}>
         {children}
       </button>
     );
