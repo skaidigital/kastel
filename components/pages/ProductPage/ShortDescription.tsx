a449be1101ebd742b77bc9210905f51235970998
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-05-15 13:31:51 +0200

    feat(product page): hide short description on down scroll to reveal add to cart button

---
+++ components/pages/ProductPage/ShortDescription.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Text } from '@/components/base/Text';
+import { useProductPageContext } from '@/components/pages/ProductPage/Context';
+import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
+
+export function ProductPageShortDescription({
+  description,
+  className
+}: {
+  description: string;
+  className?: string;
+}) {
+  const { showProductDescription } = useProductPageContext();
+
+  return (
+    <motion.div
+      animate={showProductDescription ? { height: 'auto', opacity: 1 } : { height: 0, opacity: 0 }}
+      transition={{ duration: 0.3 }}
+      style={{ overflow: 'hidden' }}
+      className={cn(!showProductDescription && '-mt-4', className)}
+    >
+      <Text as="p" size="sm">
+        {description}
+      </Text>
+    </motion.div>
+  );
+}
