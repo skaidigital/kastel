1d56973a7ceceb48b22135d57d300d670b94d63b
“Petter <petter@skaidigital.com>
2024-06-07 09:04:14 +0200

    fix: fixed review logic for lipscore

--- components/lipscore/LipscoreInit.tsx
+++ components/lipscore/LipscoreInit.tsx
@@ -5,22 +5,23 @@ import { useEffect } from 'react';
 
 export default function LipscoreInit() {
   useEffect(() => {
-    // Dynamically load the Lipscore script and initialize
-    const script = document.createElement('script');
-    script.src = '//static.lipscore.com/assets/no/lipscore-v1.js';
-    script.async = true;
-    script.onload = () => {
+    window.lipscoreInit = function () {
       window?.lipscore?.init({
         apiKey: env.NEXT_PUBLIC_LIPSCORE_API_KEY
       });
-      window?.lipscore?.reInitWidgets(true);
     };
+    const scr = document.createElement('script');
+    scr.async = true;
+    scr.src = 'https://static.lipscore.com/assets/no/lipscore-v1.js';
+    document.head.appendChild(scr);
 
-    document.head.appendChild(script);
+    scr.onload = () => {
+      window.lipscore?.reInitWidgets(true);
+    };
 
     // Cleanup function to remove the script when the component unmounts
     return () => {
-      document.head.removeChild(script);
+      document.head.removeChild(scr);
     };
   }, []); // Empty array ensures this effect runs only once after the initial render
 
